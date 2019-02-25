359352d1f0766cee9fe128a0d28a26eeb492f0c8
ledocod <ledocoding@gmail.com>
2019-02-25 17:26:17 +0000

    first

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -16,7 +16,7 @@ import {
 } from '../utils/i18n';
 
 const GITHUB_USERNAME = 'gaearon';
-const GITHUB_REPO_NAME = 'overreacted.io';
+const GITHUB_REPO_NAME = 'indesign.io';
 const systemFont = `system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI",
     "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans",
     "Droid Sans", "Helvetica Neue", sans-serif`;
@@ -133,7 +133,7 @@ class BlogPostTemplate extends React.Component {
       enSlug.length - 1
     )}/index${lang === 'en' ? '' : '.' + lang}.md`;
     const discussUrl = `https://mobile.twitter.com/search?q=${encodeURIComponent(
-      `https://overreacted.io${enSlug}`
+      `https://indesign.io${enSlug}`
     )}`;
 
     return (
