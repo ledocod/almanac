e1b9c2f4c99c8b52ad917123607a9e405282d194
ledocod <ledocoding@gmail.com>
2019-03-25 22:25:46 +0000

    colors

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -24,7 +24,7 @@ class Bio extends React.Component {
         />
         <p style={{fontFamily: 'Inter, sans-serif', maxWidth: 310 }}>
           Personal blog by{' '}
-          <a href="#">Eugene Lazarev</a>.{' '}
+          <a href="https://elazarev.me">Eugene Lazarev</a>.{' '}
           I&nbsp;explain with words and code.
         </p>
       </div>
